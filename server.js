05c4adc0009e6e58df5495d57cee502bb9182239
Vijay <vijay@pluto7.com>
2017-04-25 22:52:53 -0700

    Add DELETE todos route

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/todos', function (req, res) {
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {id: todoId});
-	
+
 	if (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
@@ -30,21 +30,53 @@ app.get('/todos/:id', function (req, res) {
 	}
 });
 
+// POST /todos
 app.post('/todos', function (req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
-
 
 	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
 		return res.status(400).send();
 	}
 
-	body.description = body.description.trim();
-
+	body.description = body.description.trim();	
 	body.id = todoNextId++;
+
 	todos.push(body);
+	
 	res.json(body);
+});
+
+// DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if (!matchedTodo) {
+		res.status(404).json({"error": "no todo found with that id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
 });
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
