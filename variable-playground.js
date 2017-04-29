a1acd3ccb15ff476cdd17a26fccc66885d684e3f
Vijay <vijay@pluto7.com>
2017-04-28 20:41:02 -0700

    Add variable-playground

---
+++ variable-playground.js
@@ -0,0 +1,25 @@
+// var person = {
+// 	name: 'Andrew',
+// 	age: 21
+// };
+
+// function updatePerson(obj) {
+// 	// obj = {
+// 	// 		name: 'Andrew',
+// 	// 		age: 24
+// 	// };
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+var grades =[15, 88];
+
+function addGrades(grades) {
+ grades.push(55);
+ debugger;
+}
+addGrades(grades);
+console.log(grades);
